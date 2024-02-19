5fb603055b6301d59682c52e4eb9a50ee8e8dba1
cevival <cevigui33@gmail.com>
2024-02-19 15:25:57 +0100

    Classe apple
    création de la classe + modif pour le main

--- base_travail_5.py
+++ base_travail_5.py
@@ -3,26 +3,25 @@ import random
 from labyrinthe import Labyrinthe
 from grid import Grid
 from utils import Pos
-
-
+from apple import Pommes 
 
 # pygame setup
 pygame.init()
 
-#constantes
-tilesize = 32 # taille d'une tuile IG
-size = (20, 10) # taille du monde
-fps = 30 # fps du jeu
-player_speed = 150 # vitesse du joueur
-next_move = 0 # tic avant déplacement
+# constantes
+tilesize = 32  # taille d'une tuile IG
+size = (20, 10)  # taille du monde
+fps = 30  # fps du jeu
+player_speed = 150  # vitesse du joueur
+next_move = 0  # tic avant déplacement
 
 # color
 color = {
-    "ground_color" : "#EDDACF",
-    "grid_color" : "#7F513D",
-    "head_color" : "#FF0000",
-    "body_color" : "#00FF00",
-    "wall_color" : "#000000"
+    "ground_color": "#EDDACF",
+    "grid_color": "#7F513D",
+    "head_color": "#FF0000",
+    "body_color": "#00FF00",
+    "wall_color": "#000000"
 }
 
 level = "data/laby-01.dat"
@@ -31,10 +30,10 @@ laby = Labyrinthe(size[0], size[1])
 laby.load_from_file(level)
 laby.set_color(color["wall_color"])
 
-grid = Grid(size[0], size[1],tilesize)
+grid = Grid(size[0], size[1], tilesize)
 grid.set_color(color["grid_color"])
 
-screen = pygame.display.set_mode((size[0]*tilesize, size[1]*tilesize))
+screen = pygame.display.set_mode((size[0] * tilesize, size[1] * tilesize))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -42,71 +41,73 @@ dt = 0
 show_grid = True
 show_pos = False
 
-keys= { "UP":0 , "DOWN":0, "LEFT":0, "RIGHT":0 }
+keys = {"UP": 0, "DOWN": 0, "LEFT": 0, "RIGHT": 0}
 
-player_pos = Pos(9,4)
-body_pos = Pos(9,3)
+player_pos = Pos(9, 4)
+body_pos = Pos(9, 3)
 
 z_press = 1
 s_press = 0
 q_press = 0
 d_press = 0
 
-#tour de boucle, pour chaque FPS
-while running:
+pommes = Pommes(screen, laby, tilesize)
 
-    #
-    #   Gestion des I/O  
-    #
-    
-    #   lecture clavier / souris
+# placer les pommes initiales
+for _ in range(3):
+    pommes.placer_pomme()
+
+# tour de boucle, pour chaque FPS
+while running:
+    # gestion des I/O
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_z or event.key == pygame.K_UP:
                 keys['UP'] = 1
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 keys['DOWN'] = 1
-            if event.key == pygame.K_q or event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_q or event.key == pygame.K_LEFT:
                 keys['LEFT'] = 1
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 keys['RIGHT'] = 1
-            if event.key == pygame.K_a:  # Quitter le jeu avec la touche A
+            elif event.key == pygame.K_a:  # Quitter le jeu avec la touche A
                 running = False
-            if event.key == pygame.K_p:  # Mettre en pause avec la touche P
-                pause = not pause
-
-        if event.type == pygame.KEYUP:
+            elif event.key == pygame.K_p:
+                Pause = True
+                while Pause:
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                            Pause = False
+                            # modif après édition
+                            fps = 0
+                            dt = 999
+                            break
+        elif event.type == pygame.KEYUP:
             if event.key == pygame.K_z or event.key == pygame.K_UP:
                 keys['UP'] = 0
                 next_move = 1
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 keys['DOWN'] = 0
                 next_move = 1
-            if event.key == pygame.K_q or event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_q or event.key == pygame.K_LEFT:
                 keys['LEFT'] = 0
                 next_move = 1
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 keys['RIGHT'] = 0
                 next_move = 1
-
-            if event.key == pygame.K_ESCAPE:
+            elif event.key == pygame.K_ESCAPE:
                 running = False
-            if event.key == pygame.K_g:
+            elif event.key == pygame.K_g:
                 show_grid = not show_grid
-            if event.key == pygame.K_p:
+            elif event.key == pygame.K_p:
                 show_pos = not show_pos
-        
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             print("mouse_pos:", pos)
 
-    #
     # gestion des déplacements
-    #
-
     next_move += dt
     if next_move > 0:
         body_pos.x, body_pos.y = player_pos.x, player_pos.y
@@ -131,17 +132,15 @@ while running:
             s_press = 0
             q_press = 0
             d_press = 1
-            
 
         if z_press == 1:
-            new_y -=1
+            new_y -= 1
         elif s_press == 1:
-            new_y +=1
+            new_y += 1
         elif q_press == 1:
-            new_x -=1
-        elif d_press ==1:
-            new_x +=1
-
+            new_x -= 1
+        elif d_press == 1:
+            new_x += 1
 
         # Gérer les bords du labyrinthe de manière circulaire
         new_x %= size[0]
@@ -152,12 +151,7 @@ while running:
             player_pos.x, player_pos.y = new_x, new_y
             next_move -= player_speed
 
-        if show_pos:
-            print("pos: ", player_pos)
-
-    #
-    # affichage des différents composants graphique
-    #
+    # affichage des différents composants graphiques
     screen.fill(color["ground_color"])
 
     laby.draw(screen, tilesize)
@@ -165,12 +159,26 @@ while running:
     if show_grid:
         grid.draw(screen)
 
-    pygame.draw.rect(screen, color["head_color"], pygame.Rect(player_pos.x*tilesize, player_pos.y*tilesize, tilesize, tilesize))
-    pygame.draw.rect(screen, color["body_color"], pygame.Rect(body_pos.x*tilesize, body_pos.y*tilesize, tilesize, tilesize*1))
+    pygame.draw.rect(screen, color["head_color"], pygame.Rect(player_pos.x * tilesize, player_pos.y * tilesize, tilesize, tilesize))
+    pygame.draw.rect(screen, color["body_color"], pygame.Rect(body_pos.x * tilesize, body_pos.y * tilesize, tilesize, tilesize * 1))
 
-    # affichage des modification du screen_view
+    # affichage des pommes
+    pommes.afficher_pommes()
+
+    # vérification de la collision avec les pommes
+    if pommes.ramasser_pomme(player_pos):
+        player_speed += 10  # Augmenter la vitesse du joueur lorsqu'il ramasse une pomme
+        pommes.placer_pomme()  # Placer une nouvelle pomme après avoir ramassé une pomme
+
+    # vérification de la fin du jeu
+    if pommes.check_fin_jeu():
+        running = False
+
+    # affichage des modifications du screen_view
     pygame.display.flip()
     # gestion fps
     dt = clock.tick(fps)
 
+# affichage du score à la fin du jeu
+pommes.afficher_score()
 pygame.quit()
